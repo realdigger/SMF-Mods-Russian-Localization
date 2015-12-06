40d0cbf271131ada418a52ff2c10ae1e7bf51c05
realdigger <neo@samtel.ru>
2015-12-06 18:15:11 +0400

    Add some translations

---
+++ P/Post and PM Inline Attachments/InlineAttachments.russian-utf8.php
@@ -0,0 +1,25 @@
+<?php
+/**********************************************************************************
+* InlineAttachments.english.php - English language file
+***********************************************************************************
+* This mod is licensed under the 2-clause BSD License, which can be found here:
+*	http://opensource.org/licenses/BSD-2-Clause
+***********************************************************************************
+* This program is distributed in the hope that it is and will be useful, but      *
+* WITHOUT ANY WARRANTIES; without even any implied warranty of MERCHANTABILITY    *
+* or FITNESS FOR A PARTICULAR PURPOSE.                                            *
+**********************************************************************************/
+if (!defined('SMF')) 
+	die('Hacking attempt...');
+
+$txt['ila_insert'] = 'Вставить вложение %d';
+$txt['ila_attachment'] = ' [ Вложение ] ';
+$txt['ila_nopermission'] = ' [ У вас нет прав на просмотр вложений ] ';
+$txt['ila_invalid'] = ' [ Сбой вложения ] ';
+$txt['ila_unapproved'] = ' [ Вложение еще не одобрено ] ';
+$txt['ila_not_uploaded'] = ' [ Вложение еще не загружено ] ';
+$txt['ila_pdf1'] = 'Не обнаружена поддержка PDF в браузере.';
+$txt['ila_pdf2'] = 'Загрузить PDF по ссылке.';
+$txt['ila_no_video'] = 'Нет возможности отобразить видео, загрузите его по ссылке ниже.';
+
+?>
---
+++ S/SMF Post Prefix/PostPrefix.russian-utf8.php
@@ -0,0 +1,104 @@
+<?php
+
+/**
+ * @package SMF Post Prefix
+ * @version 1.0
+ * @author Diego Andrés <diegoandres_cortes@outlook.com>
+ * @copyright Copyright (c) 2015, Diego Andrés
+ * @license http://www.mozilla.org/MPL/MPL-1.1.html
+ */
+
+// Admin Tabs
+$txt['PostPrefix_main'] = 'Префиксы тем';
+$txt['PostPrefix_tab_general'] = 'General';
+$txt['PostPrefix_tab_general_desc'] = 'Find out what Post Prefixes is about';
+$txt['PostPrefix_tab_prefixes'] = 'Prefixes';
+$txt['PostPrefix_tab_prefixes_desc'] = 'The list of the available prefixes';
+$txt['PostPrefix_tab_prefixes_add'] = 'Add Prefix';
+$txt['PostPrefix_tab_prefixes_add_desc'] = 'Add a new prefix, you can set the name, color and select the allowed boards and groups.';
+$txt['PostPrefix_tab_prefixes_edit_desc'] = 'Edit the current prefix, you can set the name, color and select the allowed boards and groups.';
+$txt['PostPrefix_tab_prefixes_edit'] = 'Edit Prefix';
+$txt['PostPrefix_tab_require'] = 'Require';
+$txt['PostPrefix_tab_require_desc'] = 'Here you can select on which boards the prefix will be required.';
+$txt['PostPrefix_tab_permissions'] = 'Permissions';
+$txt['PostPrefix_tab_permissions_desc'] = 'Permissions for Post Prefix mod.';
+$txt['PostPrefix_tab_settings'] = 'Settings';
+$txt['PostPrefix_tab_settings_desc'] = 'General settings for Post Prefix mod.';
+
+// General
+$txt['PostPrefix_donate_title'] = 'Donate to the author';
+$txt['PostPrefix_version'] = 'PostPrefix version';
+$txt['PostPrefix_main_credits'] = 'Credits';
+
+// Prefixes
+$txt['PostPrefix_no_prefixes'] = 'You have not added any prefix';
+$txt['PostPrefix_prefix_name'] = 'Name';
+$txt['PostPrefix_prefix_id'] = 'ID';
+$txt['PostPrefix_prefix_icon'] = 'Icon';
+$txt['PostPrefix_prefix_modify'] = 'Modify';
+$txt['PostPrefix_prefix_status'] = 'Status';
+$txt['PostPrefix_prefix_date'] = 'Date';
+$txt['PostPrefix_prefix_groups'] = 'Groups';
+
+// Add/Edit
+$txt['PostPrefix_prefix_enable'] = 'Enable prefix?';
+$txt['PostPrefix_prefix_color'] = 'Use color?';
+$txt['PostPrefix_prefix_icon_use'] = 'Use icon instead of text?';
+$txt['PostPrefix_prefix_icon_url'] = 'Set the icon URL';
+$txt['PostPrefix_add_prefix'] = 'Add prefix';
+$txt['PostPrefix_save_prefix'] = 'Update prefix';
+$txt['PostPrefix_prefix_groups'] = 'Allowed groups';
+$txt['PostPrefix_select_visible_groups'] = 'Show groups';
+$txt['PostPrefix_prefix_groups_desc'] = 'Groups allowed to use the prefix';
+$txt['PostPrefix_prefix_boards'] = 'Usable in';
+$txt['PostPrefix_select_visible_boards'] = 'Show boards';
+$txt['PostPrefix_prefix_boards_desc'] = 'Boards where the prefix can be used';
+$txt['mboards_groups_regular_members'] = 'This group contains all members that have no primary group set.';
+$txt['mboards_groups_post_group'] = 'This group is a post count based group.';
+$txt['PostPrefix_use_bgcolor'] = 'Use color as background color?';
+$txt['PostPrefix_prefix_boards_require'] = 'Require prefix on';
+$txt['PostPrefix_prefix_boards_require_desc'] = 'Boards where the prefix will be required';
+$txt['PostPrefix_required_updated'] = 'Required boards were successfully updated.';
+$txt['PostPrefix_prefix_added'] = 'The prefix %s was added successfully.';
+$txt['PostPrefix_prefix_updated'] = 'The prefix %s was updated successfully.';
+$txt['PostPrefix_prefix_delete_sure'] = 'Are you sure you want to delete the selected prefixes?';
+$txt['PostPrefix_prefix_deleted'] = 'The chosen prefixes were deleted successfully.';
+
+// Error
+$txt['PostPrefix_error_noprefix'] = 'You need to specify at least the name of the prefix.';
+$txt['PostPrefix_empty_groups'] = 'There are no groups selected for this prefix.';
+$txt['PostPrefix_empty_boards'] = 'There are no boards selected for this prefix.';
+$txt['PostPrefix_error_unable_tofind'] = 'Unable to find a prefix';
+$txt['error_no_prefix'] = 'No prefix was selected.';
+
+// Permissions
+$txt['permissionname_manage_prefixes'] = 'Manage Prefixes';
+$txt['groups_manage_prefixes'] = 'Manage Prefixes';
+$txt['permissionhelp_manage_prefixes'] = 'If the user can manage prefixes.';
+$txt['permissionhelp_groups_manage_prefixes'] = 'If the user can manage prefixes.';
+$txt['cannot_manage_prefixes'] = 'You\'re not allowed to manage the prefixes.';
+$txt['permissionname_set_prefix'] = 'Use Prefixes';
+$txt['groups_set_prefix'] = 'Use Prefixes';
+$txt['permissionhelp_set_prefix'] = 'If the user can set prefixes.';
+$txt['permissionhelp_groups_set_prefix'] = 'If the user can set prefixes.';
+$txt['cannot_set_prefix'] = 'You\'re not allowed to use prefixes.';
+
+// Settings
+$txt['PostPrefix_enable_filter'] = 'Enable filter prefix';
+$txt['PostPrefix_enable_filter_desc'] = 'It will show a box allowing you to filter topics by prefix.';
+$txt['PostPrefix_select_order'] = 'Prefixes order';
+$txt['PostPrefix_select_order_desc'] = 'The order that the prefixes list will have when creating a topic.';
+$txt['PostPrefix_select_order_dir'] = 'Order direction';
+$txt['PostPrefix_select_order_dir_desc'] = 'DESC, ASC';
+$txt['PostPrefix_DESC'] = 'DESC';
+$txt['PostPrefix_ASC'] = 'ASC';
+
+// Post
+$txt['PostPrefix_select_prefix'] = 'Префикс темы';
+$txt['PostPrefix_prefix'] = 'Префикс';
+$txt['PostPrefix_prefix_none'] = '[Не задан]';
+
+// Filter by prefix
+$txt['PostPrefix_filter'] = 'Фильтр по префиксам';
+$txt['PostPrefix_filter_noprefix'] = 'Без префиксов';
+$txt['PostPrefix_filter_all'] = 'Показать все темы';
