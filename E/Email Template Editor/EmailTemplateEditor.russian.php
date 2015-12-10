016e1b727b729518bef41ad788c799e7beb42547
realdigger <neo@samtel.ru>
2015-12-10 16:06:36 +0400

    Add Email Template Editor mod  translation

---
+++ E/Email Template Editor/EmailTemplateEditor.russian.php
@@ -0,0 +1,53 @@
+<?php
+// Labradoodle-360; Email Template Editor - EmailTemplateEditor.russian.php
+
+global $scripturl;
+$txt['email_template_editor'] = 'Редактор шаблонов уведомлений';
+$txt['email_template_editor_acp_desc'] = 'Редактор шаблонов позволяет изменить текст стандартных уведомлений, рассылаемых форумом в различных ситуациях.';
+$txt['email_template_select'] = 'Выбор шаблона';
+$txt['email_template_editor_noid'] = 'ID либо не указан, либо неверен. Попытайтесь снова.';
+
+/* New Template? */
+$txt['email_template_custom_from'] = 'Создание шаблонов';
+$txt['email_template_custom_create'] = 'Создать';
+
+/* List Items */
+$txt['email_template_name'] = 'Название';
+$txt['email_template_subject'] = 'Заголовок';
+$txt['email_template_body'] = 'Содержание';
+$txt['email_template_actions'] = 'Действия';
+$txt['email_template_empty_list'] = 'Пока никаких шаблонов нет.';
+$txt['email_template_list_header'] = 'Шаблоны';
+$txt['email_template_save'] = 'Сохранить';
+
+/* Test Email */
+$txt['email_template_test_email'] = 'Тестирование';
+$txt['email_template_select'] = 'Выберите шаблон';
+$txt['email_template_recipient'] = 'Адресат';
+$txt['email_template_send'] = 'Отправить';
+$txt['email_template_test_help'] = 'Примечание: в письмах обрабатываются только <strong>глобальные переменные</strong> (<a href="'. $scripturl. '?action=helpadmin;help=email_template_help_global_presets" onclick="return reqWin(this.href);">?</a>).';
+
+/* Add and Modify */
+$txt['email_template_global_presets'] = 'Глобальные переменные';
+
+/* Sub-Actions */
+$txt['email_template_add'] = 'Добавление';
+$txt['email_template_modify'] = 'Правка';
+$txt['email_template_delete'] = 'Удалить';
+
+/* Notification Messages */
+$txt['email_template_noti_true'] = 'Шаблон успешно';
+$txt['email_template_noti_false'] = 'Создание невозможно';
+$txt['email_template_noti_added'] = 'добавлен.';
+$txt['email_template_noti_deleted'] = 'удален.';
+$txt['email_template_noti_modified'] = 'изменен.';
+$txt['email_template_noti_true_emailsent'] = 'Тестовое сообщение успешно отправлено.';
+$txt['email_template_noti_false_emailsent'] = 'Тестовое сообщение не может быть отправлено. Убедитесь, что поле "Адресат" заполнено.';
+
+$txt['email_template_success'] = 'Успешно';
+$txt['email_template_error'] = 'Ошибка';
+
+/* Copyright; DO NOT MODIFY OR REMOVE */
+$txt['email_template_copyright'] = 'Email Template Editor &ndash; Matthew Kerle &copy; 2011 | Русский перевод &ndash; <a href="http://dragomano.ru" target="_blank">Bugo</a>';
+
+?>
