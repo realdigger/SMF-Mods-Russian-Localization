e304bd8826105a631ba43ac0d5e82119cbcbf6c9
realdigger <neo@samtel.ru>
2016-10-12 22:34:19 +0400

    Add Show Custom Membergroups translation

---
+++ S/Show Custom Membergroups/showCustomMembergroups.russian-utf8.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ *
+ * @package showCustomMembergroups mod
+ * @version 1.0
+ * @author Jessica Gonz?lez <suki@missallsunday.com>
+ * @copyright Copyright (c) 2013, Jessica Gonz?lez
+ * @license http://www.mozilla.org/MPL/2.0/
+ */
+
+global $txt;
+
+$txt['sCM_title'] = 'Показ дополнительных групп';
+$txt['sCM_setting_pageTitle'] = 'SCM панель управления';
+$txt['sCM_groups_ids'] = 'Укажите ID групп, которые вы хотите показывать для всех пользователей';
+$txt['sCM_groups_ids_sub'] = 'разделяйте запятой если несколько групп (1,2,3,4) или оставьте пустым чтобы показвать все группы.';
+$txt['sCM_ui'] = 'Дополнительные группы';
