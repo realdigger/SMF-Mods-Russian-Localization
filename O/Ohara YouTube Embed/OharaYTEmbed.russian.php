59c7c72a1cf8b267d9ef55258124a1be21ac6bee
realdigger <neo@samtel.ru>
2016-10-12 22:16:18 +0400

    Add Ohara YouTube Embed translation

---
+++ O/Ohara YouTube Embed/OharaYTEmbed.russian.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * @package Ohara Youtube Embed mod
+ * @version 2.1
+ * @author Jessica Gonzalez <missallsunday@simplemachines.org>
+ * @copyright Copyright (c) 2016 Jessica Gonzalez
+ * @license http://www.mozilla.org/MPL/ MPL 2.0
+ */
+global $txt;
+$txt['OharaYTEmbed_enable'] = 'Включить мод Ohara';
+$txt['OharaYTEmbed_enable_sub'] = 'Это основная опция, она должна быть включена для работы мода.';
+$txt['OharaYTEmbed_autoEmbed'] = 'Включить автовставку видео';
+$txt['OharaYTEmbed_autoEmbed_sub'] = 'Если включено, все правильные и доступные ссылки на видео автоматически будут преобразованы в видеоролики и вставлены в сообщение. Если отключено, используйте ББ-коды для преобразования ссылок в видеоролики.';
+$txt['OharaYTEmbed_width'] = 'Ширина видео:';
+$txt['OharaYTEmbed_width_sub'] = 'Если не задано, по умолчанию будет использовано: 420.';
+$txt['OharaYTEmbed_height'] = 'Высота видео:';
+$txt['OharaYTEmbed_height_sub'] = 'Если не задано, по умолчанию будет использовано: 315';
+$txt['OharaYTEmbed_invalid_link'] = 'Неверная ссылка с {site}';
+$txt['OharaYTEmbed_title'] = 'Ohara Embed';
+$txt['OharaYTEmbed_disabled_generic'] = 'Поддержка сайта {site} сейчас отключена';
+$txt['OharaYTEmbed_enable_generic'] = 'Включить {site}';
+$txt['OharaYTEmbed_desc_generic'] = 'Встраивать видео с {site}';
+$txt['OharaYTEmbed_modName'] = 'Ohara (auto)Embed mod';
