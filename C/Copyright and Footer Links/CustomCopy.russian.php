ff748c5eba7315599b3de2de13b65baccb283735
realdigger <neo@samtel.ru>
2016-10-12 22:51:27 +0400

    Add Copyright and Footer Links translation

---
+++ C/Copyright and Footer Links/CustomCopy.russian.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @package Custom Copyright mod
+ * @version 3.2
+ * @author Jessica Gonz?lez <missallsunday@simplemachines.org>
+ * @copyright Copyright (c) 2011, Jessica Gonz?lez
+ * @license http://www.mozilla.org/MPL/MPL-1.1.html
+ */
+/*
+ * Version: MPL 1.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is http://missallsunday.com code.
+ *
+ * The Initial Developer of the Original Code is
+ * Jessica Gonz?lez.
+ * Portions created by the Initial Developer are Copyright (C) 2011
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *
+ */
+global $txt;
+$txt['CFL_enable'] = 'Включить мод Custom Footer and Links.';
+$txt['CFL_enable_sub'] = 'Опция должна быть включена, чтобы использовать мод.';
+$txt['CFL_foot_links'] = 'Введите здесь свою строку с копирайтами или ссылками:';
+$txt['CFL_foot_links_sub'] = 'Можно использовать только HTML, бб-коды не поддерживаются.';
