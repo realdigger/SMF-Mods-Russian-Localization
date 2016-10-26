b941d7f486d7e490999b19967905e6a4ec231acb
realdigger <neo@samtel.ru>
2016-10-26 06:53:20 +0400

    Add Activity Bar translation

---
+++ A/ActivityBar/ActivityBar.russian-utf8.php
@@ -0,0 +1,28 @@
+<?php
+
+global $txt;
+
+$txt['ActivityBar_standardlabel'] = 'Индикатор активности';
+$txt['ActivityBar_master'] = 'Включить индикатор активности';
+$txt['ActivityBar_master_sub'] = 'Опция должна быть включена для активации мода.';
+$txt['ActivityBar_label'] = 'Подпись индикатора';
+$txt['ActivityBar_label_sub'] = 'Подпись отображаемая с индикатором. По умолчанию "'.$txt['ActivityBar_standardlabel'].'".';
+$txt['ActivityBar_timeframe'] = 'Период активности';
+$txt['ActivityBar_timeframe_sub'] = 'Подсчитываются сообщения сделанные за указанный период (в днях). <br />. По умолчанию 30.';
+$txt['ActivityBar_show_in_posts'] = 'Показывать индикатор активности в сообщениях';
+$txt['ActivityBar_show_in_posts_sub'] = 'Для отображения индикатора под аватаром в каждом сообщении.';
+$txt['ActivityBar_show_in_profile'] = 'Показывать индикатор активности в профилях';
+$txt['ActivityBar_show_in_profile_sub'] = 'Для отображения индикатора в профиле пользователя.';
+$txt['ActivityBar_max_posts'] = 'Максимум сообщений';
+$txt['ActivityBar_max_posts_sub'] = 'Количество сообщений при котором индикатор полностью заполнится. <br />Оставьте пустым, чтобы использовать значение по умолчанию: (500 сообщений).';
+$txt['ActivityBar_max_width'] = 'Максимальная ширина';
+$txt['ActivityBar_max_width_sub'] = 'Ширина индикатора. При достижении заданного лимита сообщений, индикатор будет не больше заданной ширины (в пикселах). <br />Оставьте пустым, чтобы использовать значение по умолчанию: (139px).';
+$txt['ActivityBar_placement'] = 'Расположение';
+$txt['ActivityBar_placement_sub'] = 'Где отображать индикатор.';
+$txt['ActivityBar_modName'] = 'Activity Bar';
+$txt['ActivityBar_colors'] = 'Использовать цветные индикаторы';
+$txt['ActivityBar_colors_sub'] = 'Если включено, будут использованы разные цвета для индикаторов, в зависимости от процентов:<br>
+0 to 33% : зеленый<br>
+34 to 66% : желтый<br>
+больше 67% : красный<br>
+Если не включено, все будут синими.';
