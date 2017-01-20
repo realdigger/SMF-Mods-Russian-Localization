15b17a0bf15edd1f221e764144354d1052825db8
realdigger <neo@samtel.ru>
2017-01-20 22:06:35 +0400

    Add PM Ultimate Profile  translation

---
+++ U/Ultimate Profile/README.md
@@ -0,0 +1,13 @@
+[mod_title] Ultimate Profile  
+[mod_id] JovanT:ultimate_profile  
+[mod_link] http://custom.simplemachines.org/mods/index.php?mod=1675  
+[mod_version] 0.9.1  
+[mod_ru]  
+[author] Bugo  
+[link] http://dragomano.ru    
+[license]   
+[conditions] http://dragomano.ru/about                             
+[win1251] да  
+[utf8] да  
+[type] мод  
+[comment] Запаковать в архив и установить как обычный мод
---
+++ U/Ultimate Profile/UltimateProfile.russian.php
@@ -0,0 +1,155 @@
+<?php
+// Version: 0.9.x; UltimateProfile
+// This file is a part of Ultimate Profile mod
+// Translation by Bugo (http://dragomano.ru/)
+
+$txt['ultimate_profile'] = 'Расширенный профиль';
+
+$txt['pictures'] = 'Изображения';
+$txt['customized'] = $txt['ultimate_profile'];
+$txt['buddies'] = 'Друзья';
+
+$txt['profile_customized'] = $txt['ultimate_profile'];
+$txt['profile_customized_info'] = 'Здесь вы можете рассказать больше о себе. Не любите делиться личной информацией — не пишите ничего. Поле не обязательно.';
+
+$txt['profile_customize_enable'] = 'Включить расширенный профиль?';
+$txt['profile_customize_private'] = 'Только друзья могут просматривать мой расширенный профиль';
+$txt['profile_comments_disable'] = 'Отключить комментарии';
+$txt['profile_comments_budd_only'] = 'Только друзья могут оставлять комментарии';
+$txt['profile_comments_notif_disable'] = 'Отключить уведомления о новых комментариях';
+$txt['profile_pictures_budd_only'] = 'Показывать мои изображения только друзьям и админам';
+$txt['profile_num_buddies_shown'] = 'Количество друзей, показываемых в профиле';
+
+$txt['profile_about'] = 'Обо мне';
+$txt['profile_about_desc'] = 'Здесь можно написать пару слов о себе.';
+$txt['profile_interests'] = 'Интересы';
+$txt['profile_interests_desc'] = 'Музыка, фильмы, спорт, хобби...';
+$txt['profile_media'] = 'Мои фотки и видео';
+$txt['profile_media_desc'] = 'Вставьте код с видео и музыкой. HTML и CSS поддерживаются.';
+$txt['profile_css'] = 'Персонализация';
+$txt['profile_css_desc'] = 'Вы можете изменить вид вашего профиля с помощью HTML и CSS.';
+
+$txt['profile_pictures_over'] = 'Вы превысили лимит изображений в профиле.';
+$txt['profile_pictures_add_not'] = 'У Вас нет прав для добавления изображений в этот профиль.';
+$txt['profile_pictures_fields'] = 'Все поля должны быть заполнены.';
+$txt['profile_pictures_upload_fail'] = 'Загрузка изображения прошла неудачно.';
+$txt['profile_pictures_pic_not'] = 'Загружаемый файл не является изображением.';
+
+$txt['profile_pictures_edit_not'] = 'У Вас недостаточно прав для редактирования изображений в этом профиле.';
+$txt['profile_pictures_delete_not'] = 'У Вас недостаточно прав для удаления изображений в этом профиле.';
+$txt['profile_pictures_not_found'] = 'Запрашиваемое изображение не найдено.';
+
+
+$txt['profile_comment_field'] = 'Вы должны ввести комментарий.';
+
+
+$txt['profile_contact'] = 'Свяжитесь со мной :)';
+$txt['profile_buddies'] = 'Мои друзья';
+$txt['profile_buddies_no'] = 'Друзей пока нет.';
+
+$txt['profile_pictures'] = 'Мои фотки';
+$txt['profile_pictures_no'] = 'Фоток пока нет.';
+
+$txt['profile_about_me'] = 'Обо мне';
+$txt['profile_about_no'] = 'Напишу позже. :)';
+
+$txt['profile_interests'] = 'Мои интересы';
+
+$txt['profile_pictures_show'] = 'Просмотр фоток';
+$txt['profile_buddies_show'] = 'Просмотр друзей';
+
+$txt['comment'] = 'Добавить комментарий';
+$txt['profile_comment'] = 'Комментарий';
+$txt['profile_comments'] = 'Комментарии';
+$txt['profile_comment_add'] = $txt['comment'];
+$txt['profile_comment_reply'] = 'Ответить';
+$txt['profile_comment_delete'] = 'Удалить комментарий';
+$txt['profile_comment_no'] = 'Пока комментариев нет. Не хочешь добавить первый?';
+$txt['profile_comments_buddies_only'] = 'Только друзья могут оставлять здесь комментарии.';
+
+$txt['profile_picture_caption'] = 'Фотографии';
+$txt['profile_picture_title'] = 'Название';
+$txt['profile_picture_description'] = 'Описание';
+$txt['profile_picture_path'] = 'Директория для хранения фотографий';
+$txt['profile_picture_not_allowed'] = 'У Вас недостаточно прав для просмотра фотографий.';
+
+$txt['profile_picture_add'] = 'Добавить фотку';
+$txt['profile_picture_edit'] = 'Редактировать фотку';
+$txt['profile_picture_delete'] = 'Удалить фотку';
+
+$txt['profile_picture_previous'] = '&laquo; назад';
+$txt['profile_picture_next'] = 'дальше &raquo;';
+
+// %s will be replaced with member's name
+$txt['profile_picture_members'] = 'Фотографий';
+
+$txt['profile_album_title'] = 'Название';
+$txt['profile_album_basic'] = 'Основной альбом';
+$txt['profile_album_parent'] = 'Корневая папка';
+$txt['profile_albums_pic_count'] = 'Фотки';
+
+$txt['profile_album_add'] = 'Добавить альбом';
+$txt['profile_album_edit'] = 'Редактировать альбом';
+$txt['profile_album_delete'] = 'Удалить альбом';
+$txt['profile_album_delete_confirm'] = 'Вы уверены? При удалении альбома будут утеряны все фотографии, находящиеся в нём!';
+
+$txt['profile_albums_add_not'] = 'У Вас недостаточно прав для добавления альбомов в этом профиле.';
+$txt['profile_albums_edit_not'] = 'У Вас недостаточно прав для редактирования альбомов в этом профиле.';
+$txt['profile_albums_delete_not'] = 'У Вас недостаточно прав для удаления альбомов в этом профиле.';
+$txt['profile_albums_fields'] = $txt['profile_pictures_fields'];
+$txt['profile_albums_parent_not'] = 'Выбранный альбом не обнаружен.';
+$txt['profile_album_not_found'] = 'Запрашиваемый альбом не обнаружен.';
+
+
+$txt['profile_enable_all'] = 'Включить расширенный профиль для всех пользователей';
+$txt['profile_enable_pictures'] = 'Включить загрузку фотографий';
+$txt['profile_pictures_path'] = 'Директория для хранения фотографий';
+$txt['profile_pictures_url'] = 'URL-адрес директории с фотографиями';
+$txt['profile_pictures_number'] = 'Количество разрешённых фотографий для пользователя<div class="smalltext">(0 - без ограничений)</div>';
+$txt['profile_pictures_width'] = 'Ширина загруженных изображений (в пикселах)';
+$txt['profile_buddies_shown'] = 'Количество друзей, показываемых в профиле (по умолчанию)';
+$txt['profile_allow_customize'] = 'Разрешить индивидуальную настройку профиля';
+$txt['profile_allow_mediabox'] = 'Разрешить раздел с фотками и видео';
+
+
+$txt['profile_notif_com_subject'] = 'Новый комментарий от %s';
+$txt['profile_notif_com_body'] = '[b]%s[/b] оставил комментарий в [url=%s]вашем профиле[/url].';
+$txt['profile_notif_com_user'] = 'Уведомление';
+
+$txt['profile_notif_piccom_subject'] = 'Новый комментарий к изображению от %s';
+$txt['profile_notif_piccom_body'] = '[b]%s[/b] оставил комментарий к Вашему изображению. [url=%s]Нажмите здесь[/url] для просмотра.';
+
+$txt['report'] = 'Сообщить модератору';
+$txt['profile_report_this'] = 'Сообщить админу';
+$txt['profile_report_text1'] = 'Пожаловаться на профиль администратору';
+$txt['profile_report_text2'] = 'Используйте эту функцию для сообщения администрации форума об оскорбительных или представляющих угрозу профилях.';
+$txt['profile_report_text3'] = 'Ваш комментарий';
+$txt['profile_report_submit'] = 'Отправить';
+$txt['profile_report_own'] = 'Хотите пожаловаться на собственный профиль?';
+
+$txt['profile_report_subject'] = 'Обжалованные профили';
+$txt['profile_report_email1'] = 'Следующий профиль, "%s"';
+$txt['profile_report_email2'] = ' был обжалован';
+$txt['profile_report_email3'] = 'на форуме, который вы модерируете';
+$txt['profile_report_comment'] = 'Комментарий пользователя';
+
+$txt['buddy_center'] = 'Друзья';
+$txt['buddy_explanation'] = 'Пользователю отправлена заявка с предложением подружиться.';
+$txt['buddy_order_up'] = 'Переместить вверх';
+$txt['buddy_order_down'] = 'Переместить вниз';
+$txt['buddy_approve'] = 'Принять дружбу';
+$txt['buddy_remove'] = 'Удалить друга';
+$txt['buddy_empty'] = 'Ваш список друзей пуст.';
+$txt['buddy_unapproved'] = 'Предложения дружбы';
+$txt['buddy_pending'] = 'Текущее';
+$txt['buddy_already_added'] = 'Пользователь уже является вашим другом.';
+
+$txt['buddy_notif_new_subject'] = '%s хочет стать вашим другом';
+$txt['buddy_notif_new_body'] = '[b]%s[/b] хочет стать вашим другом. [url=%s]Нажмите здесь[/url], чтобы принять или отклонить его/её дружбу.';
+
+$txt['buddy_top3'] = 'Топ 3';
+$txt['buddy_top6'] = 'Топ 6';
+$txt['buddy_top9'] = 'Топ 9';
+$txt['buddy_top12'] = 'Топ 12';
+
+?>
