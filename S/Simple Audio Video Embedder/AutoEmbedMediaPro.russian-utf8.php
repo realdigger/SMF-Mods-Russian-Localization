8bf7c039dd74a24fab0609a2cd830e01157ef47d
realdigger <neo@samtel.ru>
2017-09-29 20:52:26 +0400

    Add Simple Audio Video Embedder translation

---
+++ S/Simple Audio Video Embedder/AutoEmbedMediaPro.russian-utf8.php
@@ -0,0 +1,30 @@
+<?php
+/*
+Simple Audio Video Embedder
+Version 1.1
+by:vbgamer45
+http://www.smfhacks.com
+
+License Information:
+Links to http://www.smfhacks.com must remain unless
+branding free option is purchased.
+*/
+
+// Latest Version:
+$txt['mediapro_txt_latestversion'] = 'Последняя версия: ';
+$txt['mediapro_txt_yourversion'] = 'Установленная версия: ';
+$txt['mediapro_txt_version_outofdate'] = 'Используемая версия Simple Audio Video Embedder устарела!<br /><a href="http://custom.simplemachines.org/mods/index.php?mod=2681" target="_blank">Скачайте последнюю версию!</a>';
+
+$txt['mediapro_txt_settings'] = 'Настройки';
+$txt['mediapro_txt_default_width'] = 'Ширина плеера:';
+$txt['mediapro_txt_default_height'] = 'Высота плеера:';
+$txt['mediapro_txt_default_info'] = 'Укажите 0 для использования стандартных значений.';
+
+$txt['mediapro_megauploadtometoday'] = 'MegaUpload/KimDotcom';
+$txt['mediapro_megauploadsong'] = 'MegaUpload Song';
+$txt['mediapro_megamegatribute'] = 'Tribute to MegaUpload';
+$txt['mediapro_dedication'] = 'Dedicated to MegaUpload and Kim DotCom was great while it lasted and lived well.';
+$txt['mediapro_megamegacopterandfire']  = 'New Zealand Fireworks 2011';
+$txt['mediapro_megaracer']  = 'MegaRacer #1 Kills MW3';
+
+?>
---
+++ S/Simple Audio Video Embedder/README.md
@@ -0,0 +1,13 @@
+[mod_title] Simple Audio Video Embedder  
+[mod_id] vbgamer45:autoembedmediapro  
+[mod_link] https://custom.simplemachines.org/mods/index.php?mod=2681  
+[mod_version] 4.1.4  
+[mod_ru]  
+[author] Bugo  
+[link] https://dragomano.ru/translations/simple-audio-video-embedder  
+[license]  
+[conditions] http://dragomano.ru/about  
+[win1251] да  
+[utf8] да  
+[type] мод  
+[comment] Запаковать в архив и установить как мод.  
