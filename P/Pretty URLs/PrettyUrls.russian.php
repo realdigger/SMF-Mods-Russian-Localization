386ff53192a85215d61ddc029ec2fcc75645573f
realdigger <digger@mysmf.net>
2018-04-14 04:27:31 +0400

    Add Pretty URLs translation

---
+++ P/Pretty URLs/PrettyUrls.russian.php
@@ -0,0 +1,60 @@
+<?php
+//	Version: 1.0; PrettyUrls
+
+// Install chrome
+$txt['pretty_chrome_install_title'] = 'Установка Pretty URLs';
+$txt['pretty_install_success'] = 'Спасибо за использование <b>Pretty URLs</b>! Установка успешно завершена!';
+$txt['pretty_install_continue'] = 'Перейдите на страницу настроек, где вы можете включить перезапись адресов.';
+
+//	Admin chrome
+$txt['pretty_chrome_title'] = 'Управление Pretty URLs';
+$txt['pretty_chrome_menu_news'] = 'Новости';
+$txt['pretty_chrome_menu_settings'] = 'Настройки';
+$txt['pretty_chrome_menu_maintenance'] = 'Обслуживание';
+
+//	News page
+$txt['pretty_chrome_caption_news'] = 'Последние новости с сайта разработчика';
+$txt['pretty_chrome_page_title_news'] = 'Новости Pretty URLs';
+$txt['pretty_chrome_title_news'] = 'Новости';
+$txt['pretty_current_version'] = 'Установленная версия';
+$txt['pretty_download'] = 'Загрузить';
+$txt['pretty_latest_version'] = 'Новая версия';
+$txt['pretty_upgrade'] = 'Обновить!';
+$txt['pretty_version'] = 'Информация о версии';
+
+//	Settings page
+$txt['pretty_cant_write_htaccess'] = 'Невозможно перезаписать файл .htaccess. Проверьте его права на запись.';
+$txt['pretty_chrome_caption_settings'] = 'Панель настроек';
+$txt['pretty_chrome_page_title_settings'] = 'Настройки Pretty URLs';
+$txt['pretty_core_settings'] = 'Основные настройки';
+$txt['pretty_enable'] = 'Включить перезапись адресов';
+$txt['pretty_filters'] = 'Фильтры перезаписи адресов';
+$txt['pretty_save'] = 'Сохранить изменения';
+$txt['pretty_skipactions'] = 'Список игнорируемых action: ';
+$txt['pretty_skipactions_note'] = 'Введите названия action, разделенные запятой, для которых не использовать перезапись адресов';
+$txt['pretty_bufferusecache'] = 'Использовать кеширование вместо таблицы в БД для храния адресов перезаписи';
+// Tests page
+$txt['pretty_chrome_caption_tests'] = 'Перед включением перезаписи адресов, проверьте что эти ссылки работают.';
+
+//	Maintenance page
+$txt['pretty_chrome_caption_maintenance'] = 'Задачи обслуживания';
+$txt['pretty_chrome_page_title_maintenance'] = 'Обслуживание Pretty URLs';
+$txt['pretty_run_maintenance'] = 'Запустить задачи обслуживания';
+
+//	Edit filters page
+$txt['pretty_chrome_caption_filters'] = 'Инструмент для управления фильтрами. Массивы задаются в формате <a href="http://www.json.org/">JSON</a>. Будьте осторожны, парсер JSON очень чувствителен к ошибкам.';
+$txt['pretty_chrome_page_title_filters'] = 'Управление фильтрами Pretty URLs';
+$txt['pretty_chrome_title_filters'] = 'Управление фильтрами';
+$txt['pretty_no_json'] = 'Для работы фильтров требуется расширение PHP JSON. Следует обновить PHP до версии 5.2 или старше.';
+
+// Board URLs interface
+$txt['pretty_add_url'] = 'Добавить';
+$txt['pretty_add_url_description'] = 'Добавить адрес. Не зависимо от того, что вы введете, в адресе будут использованы только допустимые символы.';
+$txt['pretty_board_url_title'] = 'Pretty URLs для: ';
+$txt['pretty_deleted_board'] = 'Удален раздел #';
+$txt['pretty_duplicate_link'] = 'сначала удалите.';
+$txt['pretty_duplicate_warning'] = 'Этот адрес использовался другим разделом. Теперь он станет доступным.';
+$txt['pretty_make_primary'] = 'Сделать основным';
+$txt['pretty_no_primary_warning'] = 'Предупреждение, нет основного адреса!';
+$txt['pretty_numerical'] = 'Этот адрес является числом или совпадает с адресом раздела, ID которого добавляется в качестве суффикса.';
+$txt['pretty_primary_url'] = 'Основной';
---
+++ P/Pretty URLs/README.md
@@ -0,0 +1,12 @@
+[mod_title] Pretty URLs  
+[mod_id] el:prettyurls  
+[mod_link] http://custom.simplemachines.org/mods/index.php?mod=636  
+[mod_version] 1.1.3  
+[author] digger  
+[link] http://mysmf.ru  
+[license] CC0  
+[conditions]                            
+[win1251]  
+[utf8] да  
+[type] файл  
+[comment] Скопировать в /Themes/default/languages/
